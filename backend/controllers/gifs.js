c157182d27ecf48d0ae8b5c5cb13ac02f32bcb13
Qusai Fares <qusaifares@gmail.com>
2020-02-07 15:34:14 -0500

    all done

--- backend/controllers/gifs.js
+++ backend/controllers/gifs.js
@@ -3,14 +3,38 @@ const Gif = require('../models/schema');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  const name = req.query.name;
+  console.log(name);
   Gif.find({})
     .then(gifs => res.json(gifs))
     .catch(console.error);
 });
 
-router.get('/:name', (req, res) => {
-  Gif.find({})
-    .then(gifs => res.json(gifs))
+router.post('/', (req, res) => {
+  Gif.create({
+    name: req.body.name,
+    url: req.body.url
+  })
+    .then(gifs => {
+      Gif.find({}).then(data => res.json(data));
+    })
+    .catch(console.error);
+});
+
+router.put('/:id', (req, res) => {
+  const gif = { name: req.body.name, url: req.body.url };
+  Gif.findOneAndUpdate({ _id: req.params.id }, { $set: gif }, { new: true })
+    .then(gifs => {
+      Gif.find({}).then(data => res.json(data));
+    })
+    .catch(console.error);
+});
+
+router.delete('/:id', (req, res) => {
+  Gif.deleteOne({ _id: req.params.id })
+    .then(gifs => {
+      Gif.find({}).then(data => res.json(data));
+    })
     .catch(console.error);
 });
 
